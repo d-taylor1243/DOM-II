30b6945059834a6385e6bdc63a85b08c4587a8aa
d-taylor1243 <dtaylor1243@gmail.com>
2023-07-17 19:44:44 -0400

    updated to correct repository

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import { text } from 'express';
 import './less/index.less'
 
-// Your code goes here!
+let navBar = Array.from(document.getElementsByClassName('nav')[0].children);
+let bottomButtons = Array.from(document.getElementsByClassName('btn'));
+let body = document.getElementsByTagName('body')[0];
+let header= document.getElementsByTagName('header')[0];
+function textContentReset(str, element) {
+    element.textContent = str;    
+}
+
+//let dblClickE = function (event) {
+   // let content = event.target.textContent;
+    //let element = event.target;
+    //event.target.textContent = "You've double-clicked me!";
+    //setTimeout(textContentReset(content, element), 2000);
+//}
+
+
+navBar.forEach(child => child.addEventListener('mouseover', e => e.target.style.backgroundColor = 'green'));
+navBar.forEach(child => child.addEventListener('mouseout', e => e.target.style.backgroundColor = 'white'));
+bottomButtons.forEach(button => button.addEventListener('mousedown', e => e.target.textContent = "You are firing a mousedown event!"));
+bottomButtons.forEach(button => button.addEventListener('mouseup', e => e.target.textContent = "Sign Me Up!"));
+//click is press and release on a mousebutton, mousedown is simply press
+window.addEventListener('keydown', e => e.code == 'g' || 'G' ? body.style.backgroundColor = 'green' : '');
+window.addEventListener('load', e => console.log('page loaded! This could be a great event for running diagnostic checking functions.'));
+//header.getElementsByTagName('h1')[0].addEventListener('dblclick', e => dblClickE(e));
