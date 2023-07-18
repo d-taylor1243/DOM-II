6972fa47e7d1c064797800a3d0550e8f722edcb1
d-taylor1243 <dtaylor1243@gmail.com>
2023-07-17 20:22:54 -0400

    made progress

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import { text } from 'express';
+//import { text } from 'express';
 import './less/index.less'
 
 let navBar = Array.from(document.getElementsByClassName('nav')[0].children);
 let bottomButtons = Array.from(document.getElementsByClassName('btn'));
 let body = document.getElementsByTagName('body')[0];
-let header= document.getElementsByTagName('header')[0];
+let header = document.getElementsByTagName('header')[0];
+let pText = Array.from(document.getElementsByTagName('p'));
 function textContentReset(str, element) {
     element.textContent = str;    
 }
 
-//let dblClickE = function (event) {
-   // let content = event.target.textContent;
-    //let element = event.target;
-    //event.target.textContent = "You've double-clicked me!";
-    //setTimeout(textContentReset(content, element), 2000);
-//}
+let dblClickE = function (event) {
+    let content = event.target.textContent;
+    let element = event.target;
+    event.target.textContent = "You've double-clicked me!";
+    setTimeout(textContentReset, 2000, content, element);
+    
+}
 
 
 navBar.forEach(child => child.addEventListener('mouseover', e => e.target.style.backgroundColor = 'green'));
@@ -22,6 +24,9 @@ navBar.forEach(child => child.addEventListener('mouseout', e => e.target.style.b
 bottomButtons.forEach(button => button.addEventListener('mousedown', e => e.target.textContent = "You are firing a mousedown event!"));
 bottomButtons.forEach(button => button.addEventListener('mouseup', e => e.target.textContent = "Sign Me Up!"));
 //click is press and release on a mousebutton, mousedown is simply press
-window.addEventListener('keydown', e => e.code == 'g' || 'G' ? body.style.backgroundColor = 'green' : '');
+window.addEventListener('keydown', e => e.key == ('g' || 'G') ? body.style.backgroundColor = 'green' : body.style.backgroundColor = "");
 window.addEventListener('load', e => console.log('page loaded! This could be a great event for running diagnostic checking functions.'));
-//header.getElementsByTagName('h1')[0].addEventListener('dblclick', e => dblClickE(e));
+header.getElementsByTagName('h1')[0].addEventListener('dblclick', e => dblClickE(e));
+pText.forEach(p => p.addEventListener('select', e => {
+    e.target.textContent.color = 'red';
+    }));
